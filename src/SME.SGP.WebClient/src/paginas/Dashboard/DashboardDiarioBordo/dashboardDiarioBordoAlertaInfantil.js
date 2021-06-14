3d9961618f1b251c451174ce239fea79e5657e98
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2021-06-14 16:59:38 -0300

    Dashboard diario de bordo - ajuste regra Alerta somente turma Infantil

--- src/SME.SGP.WebClient/src/paginas/Dashboard/DashboardDiarioBordo/dashboardDiarioBordoAlertaInfantil.js
+++ src/SME.SGP.WebClient/src/paginas/Dashboard/DashboardDiarioBordo/dashboardDiarioBordoAlertaInfantil.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { AlertaPermiteSomenteTurmaInfantil } from '~/componentes-sgp';
-import { ModalidadeDTO } from '~/dtos';
 
 const DashboardDiarioBordoAlertaInfantil = () => {
-  const { modalidade } = useSelector(
+  const { ue } = useSelector(
     store => store.dashboardDiarioBordo?.dadosDashboardDiarioBordo
   );
 
   return (
     <AlertaPermiteSomenteTurmaInfantil
-      exibir={modalidade && Number(modalidade) !== ModalidadeDTO.INFANTIL}
+      exibir={ue && !ue?.ehInfantil}
       validarModalidadeFiltroPrincipal={false}
     />
   );
