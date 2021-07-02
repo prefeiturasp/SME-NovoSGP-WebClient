52642db8059b609053f5896edc85ebf21cf6129a
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2021-07-02 12:51:15 -0300

    ajuste ServicoMuralGoogleSalaAula

--- src/SME.SGP.WebClient/src/servicos/Paginas/MuralGoogleSalaAula/ServicoMuralGoogleSalaAula.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/MuralGoogleSalaAula/ServicoMuralGoogleSalaAula.js
@@ -3,7 +3,7 @@ import api from '~/servicos/api';
 const urlPadrao = '/v1/mural/avisos';
 
 class ServicoMuralGoogleSalaAula {
-  obterDadosMuralGoogleSalaAula = async aulaId => {
+  obterDadosMuralGoogleSalaAula = aulaId => {
     return api.get(`${urlPadrao}?aulaId=${aulaId}`);
   };
 }
