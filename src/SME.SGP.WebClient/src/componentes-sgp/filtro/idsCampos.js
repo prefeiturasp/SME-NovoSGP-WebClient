2feba6b8808d6b275eca38e53035e389bfca1ea6
Luiz Henrique Araújo <luizhbd@gmail.com>
2021-08-25 14:35:07 -0300

    Ajustes nos campos dos testes automatizados

---
+++ src/SME.SGP.WebClient/src/componentes-sgp/filtro/idsCampos.js
@@ -0,0 +1,7 @@
+export const SGP_BUTTON_FILTRO_PRINCIPAL_APLICAR = 'sgp-button-filtro-principal-aplicar';
+export const SGP_SELECT_FILTRO_PRINCIPAL_ANOLETIVO = 'sgp-select-filtro-principal-anoletivo';
+export const SGP_SELECT_FILTRO_PRINCIPAL_TURMA = 'sgp-select-filtro-principal-turma';
+export const SGP_SELECT_FILTRO_PRINCIPAL_MODALIDADE = 'sgp-select-filtro-principal-modalidade';
+export const SGP_SELECT_FILTRO_PRINCIPAL_PERIODO = 'sgp-select-filtro-principal-periodo';
+export const SGP_SELECT_FILTRO_PRINCIPAL_DRE = 'sgp-select-filtro-principal-dre';
+export const SGP_SELECT_FILTRO_PRINCIPAL_UE = 'sgp-select-filtro-principal-ue';
