b6ec99f21f19cc240293b101a485c2a2be5a75c7
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2021-10-04 18:25:07 -0300

    Dashboard RAA - Visão SME

---
+++ src/SME.SGP.WebClient/src/paginas/Dashboard/DashboardRelAcompAprend/TabsDashboard/Graficos/CriancasComAcompPorDRE/graficoCriancasComAcompPorDRE.js
@@ -0,0 +1,92 @@
+import PropTypes from 'prop-types';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Loader, SelectComponent } from '~/componentes';
+import { GraficoBarras } from '~/componentes-sgp';
+import { erros } from '~/servicos';
+import ServicoDashboardRelAcompanhamentoAprendizagem from '~/servicos/Paginas/Dashboard/ServicoDashboardRelAcompanhamentoAprendizagem';
+
+const GraficoTotalCriancasComAcompPorDRE = props => {
+  const { anoLetivo } = props;
+
+  const [dadosGrafico, setDadosGrafico] = useState([]);
+  const [exibirLoader, setExibirLoader] = useState(false);
+  const [semestre, setSemestre] = useState();
+
+  const obterDadosGrafico = useCallback(async () => {
+    setExibirLoader(true);
+    const retorno = await ServicoDashboardRelAcompanhamentoAprendizagem.obterTotalCriancasComAcompPorDRE(
+      anoLetivo,
+      semestre
+    )
+      .catch(e => erros(e))
+      .finally(() => setExibirLoader(false));
+
+    if (retorno?.data?.length) {
+      setDadosGrafico(retorno.data);
+    } else {
+      setDadosGrafico([]);
+    }
+  }, [anoLetivo, semestre]);
+
+  useEffect(() => {
+    if (anoLetivo && semestre) {
+      obterDadosGrafico();
+    } else {
+      setDadosGrafico([]);
+    }
+  }, [anoLetivo, semestre, obterDadosGrafico]);
+
+  const listaSemestres = [
+    { valor: 1, desc: '1° Semestre' },
+    { valor: 2, desc: '2° Semestre' },
+  ];
+
+  return (
+    <>
+      <div className="row">
+        <div className="col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-2">
+          <SelectComponent
+            id="semestre"
+            lista={listaSemestres}
+            valueOption="valor"
+            valueText="desc"
+            valueSelect={semestre}
+            onChange={valor => {
+              setSemestre(valor);
+            }}
+            placeholder="Selecione o semestre"
+            allowClear={false}
+          />
+        </div>
+      </div>
+      <Loader
+        loading={exibirLoader}
+        className={exibirLoader ? 'text-center' : ''}
+      >
+        {dadosGrafico?.length ? (
+          <GraficoBarras
+            data={dadosGrafico}
+            xField="grupo"
+            xAxisVisible
+            isGroup
+            colors={['#0288D1', '#F57C00']}
+          />
+        ) : !exibirLoader ? (
+          <div className="text-center">Sem dados</div>
+        ) : (
+          ''
+        )}
+      </Loader>
+    </>
+  );
+};
+
+GraficoTotalCriancasComAcompPorDRE.propTypes = {
+  anoLetivo: PropTypes.oneOfType(PropTypes.any),
+};
+
+GraficoTotalCriancasComAcompPorDRE.defaultProps = {
+  anoLetivo: null,
+};
+
+export default GraficoTotalCriancasComAcompPorDRE;
