6b91720c56c3d8be9111135c05f8937834ed1b51
Fernando Groeler <fernando.groeler@gmail.com>
2022-03-31 10:57:07 -0400

    Ajustado "import"

--- src/SME.SGP.WebClient/src/paginas/AEE/Plano/Cadastro/Componentes/TabCadastroPlano/tabCadastroPlano.js
+++ src/SME.SGP.WebClient/src/paginas/AEE/Plano/Cadastro/Componentes/TabCadastroPlano/tabCadastroPlano.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import * as qs from 'query-string';
 import { setDadosCollapseLocalizarEstudante } from '~/redux/modulos/collapseLocalizarEstudante/actions';
 import { setDadosObjectCardEstudante } from '~/redux/modulos/objectCardEstudante/actions';
 import {
