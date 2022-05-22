be61e550050b35262490453f79e4f0a13babf46e
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2022-05-22 18:35:05 -0300

    Atribuição de responsáveis - Nova atribuição

---
+++ src/SME.SGP.WebClient/src/paginas/Gestao/AtribuicaoSupervisor/atribuicaoResponsaveisCadastro.js
@@ -0,0 +1,426 @@
+import { Col, Row } from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import { Button, Card, Colors, Loader, SelectComponent } from '~/componentes';
+import { Cabecalho } from '~/componentes-sgp';
+import {
+  SGP_BUTTON_CANCELAR,
+  SGP_BUTTON_SALVAR,
+  SGP_BUTTON_VOLTAR,
+  SGP_SELECT_DRE,
+} from '~/componentes-sgp/filtro/idsCampos';
+import Auditoria from '~/componentes/auditoria';
+import RotasDto from '~/dtos/rotasDto';
+import { store } from '~/redux';
+import {
+  AbrangenciaServico,
+  confirmar,
+  erros,
+  history,
+  setBreadcrumbManual,
+  sucesso,
+  verificaSomenteConsulta,
+} from '~/servicos';
+import ServicoResponsaveis from '~/servicos/Paginas/Gestao/Responsaveis/ServicoResponsaveis';
+import ListaTransferenciaResponsaveis from './listaTransferenciaResponsaveis';
+
+const AtribuicaoResponsaveisCadastro = () => {
+  const { usuario } = store.getState();
+  const permissoesTela =
+    usuario.permissoes[RotasDto.ATRIBUICAO_RESPONSAVEIS] || {};
+
+  const routeMatch = useRouteMatch();
+
+  const [carregandoDres, setCarregandoDres] = useState(false);
+  const [listaDres, setListaDres] = useState([]);
+  const [dreId, setDreId] = useState();
+
+  const [tipoResponsavel, setTipoResponsavel] = useState();
+  const [listaTipoResponsavel, setListaTipoResponsavel] = useState([]);
+  const [carregandoTipoResponsavel, setCarregandoTipoResponsavel] = useState(
+    false
+  );
+
+  const [responsavel, setResponsavel] = useState();
+  const [listaResponsavel, setListaResponsavel] = useState([]);
+  const [carregandoResponsavel, setCarregandoResponsavel] = useState(false);
+
+  const [uesSemAtribuicao, setUesSemAtribuicao] = useState([]);
+  const [uesAtribuidas, setUesAtribuidas] = useState([]);
+  const [carregandoUes, setCarregandoUes] = useState(false);
+
+  const [modoEdicao, setModoEdicao] = useState(false);
+
+  const [auditoria, setAuditoria] = useState({});
+
+  useEffect(() => {
+    verificaSomenteConsulta(permissoesTela);
+  }, [permissoesTela]);
+
+  useEffect(() => {
+    if (routeMatch.params?.dreId) {
+      setBreadcrumbManual(
+        routeMatch.url,
+        'Editar Atribuição',
+        RotasDto.ATRIBUICAO_RESPONSAVEIS_LISTA
+      );
+    }
+  }, [routeMatch]);
+
+  const limparTela = () => {
+    setDreId();
+    setTipoResponsavel();
+    setListaTipoResponsavel([]);
+    setResponsavel();
+    setListaResponsavel([]);
+    setModoEdicao(false);
+  };
+
+  const salvarAtribuicao = () => {
+    const atribuicao = {
+      dreId,
+      supervisorId: responsavel,
+      uesIds: uesAtribuidas?.map?.(item => item?.codigo) || [],
+    };
+    ServicoResponsaveis.salvarAtribuicao(atribuicao)
+      .then(() => {
+        sucesso('Atribuição realizada com sucesso.');
+        history.push(RotasDto.ATRIBUICAO_RESPONSAVEIS_LISTA);
+      })
+      .catch(e => erros(e));
+  };
+
+  const onClickVoltar = async () => {
+    if (modoEdicao) {
+      const confirmado = await confirmar(
+        'Atenção',
+        '',
+        'Suas alterações não foram salvas, deseja salvar agora?'
+      );
+
+      if (confirmado) {
+        salvarAtribuicao();
+      } else {
+        history.push(RotasDto.ATRIBUICAO_RESPONSAVEIS_LISTA);
+      }
+    } else {
+      history.push(RotasDto.ATRIBUICAO_RESPONSAVEIS_LISTA);
+    }
+  };
+
+  const onClickCancelar = async () => {
+    if (modoEdicao) {
+      const confirmou = await confirmar(
+        'Atenção',
+        'Você não salvou as informações preenchidas.',
+        'Deseja realmente cancelar as alterações?'
+      );
+      if (confirmou) {
+        limparTela();
+      }
+    } else {
+      limparTela();
+    }
+  };
+
+  const onChangeDre = valor => {
+    setListaResponsavel([]);
+    setResponsavel();
+    setTipoResponsavel();
+    setDreId(valor);
+  };
+
+  const obterDres = useCallback(async () => {
+    setCarregandoDres(true);
+    const retorno = await AbrangenciaServico.buscarDres()
+      .catch(e => erros(e))
+      .finally(() => setCarregandoDres(false));
+
+    if (retorno?.data?.length) {
+      if (retorno.data.length === 1) {
+        setDreId(retorno.data[0].codigo);
+      } else if (routeMatch.params?.dreId) {
+        setDreId(routeMatch.params.dreId);
+      }
+
+      setListaDres(retorno.data);
+    } else {
+      setListaDres([]);
+      setDreId();
+    }
+  }, [routeMatch]);
+
+  useEffect(() => {
+    obterDres();
+  }, [obterDres]);
+
+  const onChangeTipoResponsavel = valor => {
+    setResponsavel();
+    setListaResponsavel([]);
+    setTipoResponsavel(valor);
+  };
+
+  const obterTipoResponsavel = useCallback(async () => {
+    setCarregandoTipoResponsavel(true);
+    const resposta = await ServicoResponsaveis.obterTipoReponsavel()
+      .catch(e => erros(e))
+      .finally(() => setCarregandoTipoResponsavel(false));
+
+    if (resposta?.data?.length) {
+      if (resposta?.data?.length === 1) {
+        setTipoResponsavel(resposta.data[0].codigo);
+      }
+
+      setListaTipoResponsavel(resposta.data);
+    } else {
+      setListaTipoResponsavel([]);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (dreId) {
+      obterTipoResponsavel();
+    } else {
+      setTipoResponsavel();
+      setListaTipoResponsavel([]);
+    }
+  }, [dreId, obterTipoResponsavel]);
+
+  const onChangeResponsavel = valor => {
+    setResponsavel(valor);
+  };
+
+  const obterResponsaveis = useCallback(async () => {
+    setCarregandoResponsavel(true);
+    const resposta = await ServicoResponsaveis.obterResponsaveis(dreId)
+      .catch(e => erros(e))
+      .finally(() => setCarregandoResponsavel(false));
+
+    if (resposta?.data?.length) {
+      if (resposta?.data?.length === 1) {
+        setResponsavel(resposta.data[0].codigo);
+      } else if (routeMatch.params?.supervisorId) {
+        setResponsavel(routeMatch.params.supervisorId);
+      }
+      setListaResponsavel(resposta.data);
+    } else {
+      setListaResponsavel([]);
+    }
+  }, [dreId, routeMatch]);
+
+  useEffect(() => {
+    if (tipoResponsavel && dreId) {
+      obterResponsaveis();
+    } else {
+      setResponsavel();
+      setListaResponsavel([]);
+    }
+  }, [dreId, tipoResponsavel, obterResponsaveis]);
+
+  const obterListaUesAtribuidas = useCallback(async () => {
+    const resposta = await ServicoResponsaveis.obterUesAtribuidas(
+      responsavel,
+      dreId
+    ).catch(e => erros(e));
+
+    if (resposta?.data?.[0]?.criadoEm) {
+      let lista = [];
+
+      if (resposta.data[0].escolas?.length) {
+        lista = resposta.data[0].escolas.map(item => {
+          return { ...item, id: item.codigo };
+        });
+      } else {
+        setUesAtribuidas([]);
+      }
+
+      setAuditoria({
+        criadoPor: resposta.data[0].criadoPor,
+        criadoEm: resposta.data[0].criadoEm,
+        criadoRf: resposta.data[0].criadoRF,
+        alteradoPor: resposta.data[0].alteradoPor,
+        alteradoEm: resposta.data[0].alteradoEm,
+        alteradoRf: resposta.data[0].alteradoRF,
+      });
+      setUesAtribuidas(lista);
+    } else {
+      setUesAtribuidas([]);
+      setAuditoria({});
+    }
+    return true;
+  }, [dreId, responsavel]);
+
+  const obterListaUES = useCallback(async () => {
+    setCarregandoUes(true);
+    const resposta = await ServicoResponsaveis.obterUesSemAtribuicao(
+      dreId
+    ).catch(e => erros(e));
+
+    if (resposta?.data?.length) {
+      const lista = resposta.data.map(item => {
+        return { ...item, id: item.codigo };
+      });
+      setUesSemAtribuicao(lista);
+    } else {
+      setUesSemAtribuicao([]);
+    }
+    if (dreId && responsavel) {
+      await obterListaUesAtribuidas();
+    }
+
+    setCarregandoUes(false);
+  }, [dreId, responsavel, obterListaUesAtribuidas]);
+
+  useEffect(() => {
+    if (dreId && responsavel) {
+      obterListaUES();
+    } else {
+      setUesSemAtribuicao([]);
+      setUesAtribuidas([]);
+    }
+  }, [dreId, responsavel, obterListaUES]);
+
+  return (
+    <>
+      <Cabecalho pagina="Atribuição de responsáveis" />
+      <Card>
+        <Col span={24}>
+          <Row gutter={[16, 8]} type="flex" justify="end">
+            <Col>
+              <Button
+                id={SGP_BUTTON_VOLTAR}
+                label="Voltar"
+                icon="arrow-left"
+                color={Colors.Azul}
+                border
+                onClick={onClickVoltar}
+              />
+            </Col>
+            <Col>
+              <Button
+                id={SGP_BUTTON_CANCELAR}
+                label="Cancelar"
+                color={Colors.Azul}
+                border
+                bold
+                onClick={() => onClickCancelar()}
+                disabled={!dreId || !modoEdicao}
+              />
+            </Col>
+            <Col>
+              <Button
+                id={SGP_BUTTON_SALVAR}
+                label="Salvar"
+                color={Colors.Roxo}
+                bold
+                onClick={() => salvarAtribuicao()}
+                disabled={
+                  !permissoesTela.podeIncluir && !permissoesTela.podeAlterar
+                }
+              />
+            </Col>
+          </Row>
+
+          <Row gutter={[16, 16]}>
+            <Col sm={24} md={12}>
+              <Loader loading={carregandoDres} ignorarTip>
+                <SelectComponent
+                  id={SGP_SELECT_DRE}
+                  label="Diretoria Regional de Educação (DRE)"
+                  lista={listaDres}
+                  valueOption="codigo"
+                  valueText="nome"
+                  disabled={
+                    !permissoesTela.podeConsultar || listaDres?.length === 1
+                  }
+                  onChange={onChangeDre}
+                  valueSelect={dreId}
+                  placeholder="Diretoria Regional De Educação (DRE)"
+                  showSearch
+                />
+              </Loader>
+            </Col>
+            <Col sm={24} md={12}>
+              <Loader loading={carregandoTipoResponsavel} ignorarTip>
+                <SelectComponent
+                  id="SGP_SELECT_TIPO_RESPONSAVEL"
+                  label="Tipo de responsável"
+                  lista={listaTipoResponsavel}
+                  valueOption="codigo"
+                  valueText="descricao"
+                  disabled={
+                    !dreId ||
+                    listaTipoResponsavel?.length === 1 ||
+                    !permissoesTela.podeConsultar
+                  }
+                  onChange={onChangeTipoResponsavel}
+                  valueSelect={tipoResponsavel}
+                  placeholder="Tipo de responsável"
+                  showSearch
+                />
+              </Loader>
+            </Col>
+            <Col sm={24} md={12}>
+              <Loader loading={carregandoResponsavel} ignorarTip>
+                <SelectComponent
+                  id="SGP_SELECT_RESPONSAVEL"
+                  label="Responsável"
+                  lista={listaResponsavel}
+                  valueOption="supervisorId"
+                  valueText="supervisorNome"
+                  disabled={
+                    !dreId ||
+                    !tipoResponsavel ||
+                    listaResponsavel?.length === 1 ||
+                    !permissoesTela.podeConsultar
+                  }
+                  onChange={onChangeResponsavel}
+                  valueSelect={responsavel}
+                  placeholder="Responsável"
+                  showSearch
+                />
+              </Loader>
+            </Col>
+            <Col sm={24}>
+              <Loader loading={carregandoUes} ignorarTip>
+                <ListaTransferenciaResponsaveis
+                  podeConsultar={permissoesTela.podeConsultar}
+                  dadosEsquerda={
+                    !carregandoUes
+                      ? uesSemAtribuicao?.length
+                        ? uesSemAtribuicao.filter(
+                            item =>
+                              !uesAtribuidas?.find?.(a => a?.id === item?.id)
+                          )
+                        : []
+                      : []
+                  }
+                  dadosDireita={!carregandoUes ? uesAtribuidas : []}
+                  setDadosEsquerda={valores => {
+                    setUesSemAtribuicao(valores);
+                    setModoEdicao(true);
+                  }}
+                  setDadosDireita={valores => {
+                    setUesAtribuidas(valores);
+                    setModoEdicao(true);
+                  }}
+                />
+              </Loader>
+            </Col>
+          </Row>
+          <Auditoria
+            className="ml-0"
+            criadoEm={auditoria.criadoEm}
+            criadoPor={auditoria.criadoPor}
+            criadoRf={auditoria.criadoRf}
+            alteradoPor={auditoria.alteradoPor}
+            alteradoEm={auditoria.alteradoEm}
+            alteradoRf={auditoria.alteradoRf}
+          />
+        </Col>
+      </Card>
+    </>
+  );
+};
+
+export default AtribuicaoResponsaveisCadastro;
---
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Gestao/Responsaveis/ServicoResponsaveis.js
@@ -0,0 +1,28 @@
+import api from '~/servicos/api';
+
+const URL_PADRAO = 'v1/supervisores';
+
+class ServicoResponsaveis {
+  obterTipoReponsavel = () => {
+    // TODO - MOCK
+    const lista = [
+      { codigo: 1, descricao: 'Supervisor Escolar' },
+      { codigo: 2, descricao: 'PAAI' },
+      { codigo: 3, descricao: 'Psicólogo Escolar' },
+      { codigo: 4, descricao: 'Psicopedagogo' },
+      { codigo: 5, descricao: 'Assistente Social' },
+    ];
+    return new Promise(resolve => resolve({ data: lista }));
+  };
+
+  salvarAtribuicao = dados => api.post(`${URL_PADRAO}/atribuir-ue`, dados);
+
+  obterResponsaveis = dre => api.get(`${URL_PADRAO}/dre/${dre}`);
+
+  obterUesSemAtribuicao = dre => api.get(`v1/dres/${dre}/ues/sem-atribuicao`);
+
+  obterUesAtribuidas = (responsavel, dre) =>
+    api.get(`${URL_PADRAO}/${responsavel}/dre/${dre}`);
+}
+
+export default new ServicoResponsaveis();
