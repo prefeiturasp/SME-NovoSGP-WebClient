60c3d302f0e5c1dcb8429a10e02501b7a0ba7dc4
guilherme <guilhermeferreira@yourdev.com.br>
2022-06-22 16:18:36 -0300

    fix: 67641

--- src/SME.SGP.WebClient/src/paginas/AEE/Plano/Cadastro/Componentes/SecaoParecerPlanoCollapse/secaoParecerPlanoCollapse.js
+++ src/SME.SGP.WebClient/src/paginas/AEE/Plano/Cadastro/Componentes/SecaoParecerPlanoCollapse/secaoParecerPlanoCollapse.js
@@ -60,11 +60,17 @@ const SecaoParecerPlanoCollapse = ({ match }) => {
         alt="secao-parecer-plano-alt"
       >
         {dadosParecer?.responsavelNome ? (
-          <div className="col-md-4">
-            <p style={{ marginLeft: -15 }}>
+          <div className="col-md-12">
+            <p
+              style={{
+                marginLeft: -15,
+                width: '100%',
+                display: 'flex',
+              }}
+            >
               PAAI responsável: {dadosParecer?.responsavelNome}
               <span
-                style={{ color: Base.CinzaDesabilitado, fontSize: '13px' }}
+                style={{ color: Base.CinzaDesabilitado, fontSize: '13px', marginLeft: 9, }}
               >{` (${dadosParecer?.responsavelRF})`}</span>
             </p>
           </div>
