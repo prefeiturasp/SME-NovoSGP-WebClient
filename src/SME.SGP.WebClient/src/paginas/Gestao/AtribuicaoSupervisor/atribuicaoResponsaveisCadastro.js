51fa6f65d476e3e605c584a42e9574fb5e7cc820
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2022-08-08 11:45:00 -0300

    Tela de Listagem e Atribuição ajuste filtros

--- src/SME.SGP.WebClient/src/paginas/Gestao/AtribuicaoSupervisor/atribuicaoResponsaveisCadastro.js
+++ src/SME.SGP.WebClient/src/paginas/Gestao/AtribuicaoSupervisor/atribuicaoResponsaveisCadastro.js
@@ -195,7 +195,6 @@ const AtribuicaoResponsaveisCadastro = () => {
   }, [routeMatch]);
   useEffect(() => {
     if (dreId) {
-      obterTipoResponsavel();
       if(dreId && responsavel != undefined){
         obterListaUES();
       }
@@ -204,7 +203,11 @@ const AtribuicaoResponsaveisCadastro = () => {
       setResponsavel();
       setListaTipoResponsavel([]);
     }
-  }, [dreId, obterTipoResponsavel]);
+  }, [dreId]);
+
+  useEffect(() => {
+    obterTipoResponsavel();
+  }, [obterTipoResponsavel]);
 
   const onChangeResponsavel = valor => {
     setResponsavel(valor);
@@ -335,6 +338,10 @@ const AtribuicaoResponsaveisCadastro = () => {
     if (!responsavel) setAuditoria({});
   }, [responsavel]);
 
+  useEffect(() => {
+    if (!responsavel) setAuditoria({});
+  }, [responsavel]);
+
   return (
     <>
       <Cabecalho pagina="Atribuição de responsáveis" />
@@ -405,7 +412,7 @@ const AtribuicaoResponsaveisCadastro = () => {
                   valueText="descricao"
                   disabled={
                     !dreId ||
-                    listaTipoResponsavel?.length === 1 ||
+                    (tipoResponsavel && listaTipoResponsavel?.length === 1) ||
                     !permissoesTela.podeConsultar
                   }
                   onChange={onChangeTipoResponsavel}
