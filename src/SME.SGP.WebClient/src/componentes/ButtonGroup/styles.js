4f1d43cc43b4cae7a3c2a29574ea4074a9abbdc0
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2022-09-05 17:23:24 -0300

    Ajuste no ButtonGroupEstilo

--- src/SME.SGP.WebClient/src/componentes/ButtonGroup/styles.js
+++ src/SME.SGP.WebClient/src/componentes/ButtonGroup/styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const ButtonGroupEstilo = styled.div`
-  padding-bottom: ${({ paddingBottom }) => `${paddingBottom} !important`};
   .btnGroupItem:not(:last-child) {
     margin-right: 16px;
   }
