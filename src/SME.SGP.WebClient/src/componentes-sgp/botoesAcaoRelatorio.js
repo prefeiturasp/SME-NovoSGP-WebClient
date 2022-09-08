c98030a6e89d034e3d3c3c5d2d76e74ad564e11a
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2022-09-08 07:40:06 -0300

    Componente botões de ação para telas de relatório

---
+++ src/SME.SGP.WebClient/src/componentes-sgp/botoesAcaoRelatorio.js
@@ -0,0 +1,94 @@
+import PropTypes from 'prop-types';
+import { Col, Row } from 'antd';
+import React from 'react';
+import {
+  SGP_BUTTON_CANCELAR,
+  SGP_BUTTON_GERAR,
+  SGP_BUTTON_VOLTAR,
+} from '~/componentes-sgp/filtro/idsCampos';
+import Button from '~/componentes/button';
+import { Colors } from '~/componentes/colors';
+import Loader from '~/componentes/loader';
+
+const BotoesAcaoRelatorio = props => {
+  const {
+    onClickVoltar,
+    onClickCancelar,
+    onClickGerar,
+    temLoaderBtnGerar,
+    desabilitarBtnGerar,
+    carregandoGerar,
+  } = props;
+
+  return (
+    <Row gutter={[8, 8]} type="flex">
+      <Col>
+        <Button
+          id={SGP_BUTTON_VOLTAR}
+          label="Voltar"
+          icon="arrow-left"
+          color={Colors.Azul}
+          border
+          onClick={() => onClickVoltar && onClickVoltar()}
+        />
+      </Col>
+      <Col>
+        <Button
+          id={SGP_BUTTON_CANCELAR}
+          label="Cancelar"
+          color={Colors.Roxo}
+          border
+          bold
+          onClick={() => onClickCancelar && onClickCancelar()}
+        />
+      </Col>
+      <Col>
+        {temLoaderBtnGerar ? (
+          <Loader loading={carregandoGerar} tip="">
+            <Button
+              id={SGP_BUTTON_GERAR}
+              icon="print"
+              label="Gerar"
+              color={Colors.Azul}
+              border
+              bold
+              onClick={() => onClickGerar && onClickGerar()}
+              disabled={desabilitarBtnGerar}
+            />
+          </Loader>
+        ) : (
+          <Button
+            id={SGP_BUTTON_GERAR}
+            icon="print"
+            label="Gerar"
+            color={Colors.Azul}
+            border
+            bold
+            onClick={onClickGerar}
+            disabled={desabilitarBtnGerar}
+          />
+        )}
+      </Col>
+    </Row>
+  );
+};
+
+BotoesAcaoRelatorio.propTypes = {
+  onClickVoltar: PropTypes.func,
+  onClickCancelar: PropTypes.func,
+  onClickGerar: PropTypes.func,
+  temLoaderBtnGerar: PropTypes.bool,
+  desabilitarBtnGerar: PropTypes.bool,
+  carregandoGerar: PropTypes.bool,
+};
+
+BotoesAcaoRelatorio.defaultProps = {
+  onClickVoltar: null,
+  onClickCancelar: null,
+  onClickGerar: null,
+  temLoaderBtnGerar: false,
+  desabilitarBtnGerar: false,
+  carregandoGerar: false,
+};
+
+export default BotoesAcaoRelatorio;
