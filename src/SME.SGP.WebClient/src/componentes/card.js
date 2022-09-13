cb9ab4a0d657b8119b57b68211b5f883da1e6817
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2022-09-13 05:38:33 -0300

    Novo estilo para o componente Card

--- src/SME.SGP.WebClient/src/componentes/card.js
+++ src/SME.SGP.WebClient/src/componentes/card.js
@@ -1,17 +1,18 @@
+import { Row } from 'antd';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 
 const CardEstilo = styled.div`
-  padding: 16px !important;
+  padding: ${props => props.padding};
   border-radius: 4px;
 `;
 
 const Card = props => {
-  const { children, className } = props;
+  const { children, className, padding } = props;
   return (
-    <CardEstilo className={`shadow bg-white ${className}`}>
-      {children}
+    <CardEstilo className={`shadow bg-white ${className}`} padding={padding}>
+      <Row>{children}</Row>
     </CardEstilo>
   );
 };
@@ -19,11 +20,13 @@ const Card = props => {
 Card.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  padding: PropTypes.string,
 };
 
 Card.defaultProps = {
   children: {},
   className: '',
+  padding: '24px 8px',
 };
 
 export default Card;
