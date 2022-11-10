40ee49555335cd569fbf3bda0bced7aec56602cc
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2022-11-10 15:32:25 -0300

    Plano AEE - parametro para impressao

--- src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/AEE/ServicoPlanoAEE.js
+++ src/SME.SGP.WebClient/src/servicos/Paginas/Relatorios/AEE/ServicoPlanoAEE.js
@@ -433,7 +433,8 @@ class ServicoPlanoAEE {
     return api.get(`${urlPadrao}/paai-ue?codigoUe=${idUe}`);
   };
 
-  imprimirVersoes = ids => api.post(`${urlPadrao}/imprimir`, ids);
+  imprimirVersoes = ids =>
+    api.post(`${urlPadrao}/imprimir`, { versoesIds: ids });
 
   removerReponsavelPAAI = id =>
     api.post(`${urlPadrao}/remover-responsavel/${id}`);
