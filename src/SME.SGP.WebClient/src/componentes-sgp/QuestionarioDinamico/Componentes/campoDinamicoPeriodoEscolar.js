705be1d858655da6b63e573e4309aefa69d820b3
Gabriel Vedolin <gabrielvedolindonascimento63@gmail.com>
2022-11-18 12:40:39 -0300

    78706 ajuste para obter o bimestre do plano

--- src/SME.SGP.WebClient/src/componentes-sgp/QuestionarioDinamico/Componentes/campoDinamicoPeriodoEscolar.js
+++ src/SME.SGP.WebClient/src/componentes-sgp/QuestionarioDinamico/Componentes/campoDinamicoPeriodoEscolar.js
@@ -16,6 +16,7 @@ const CampoDinamicoPeriodoEscolar = props => {
     onChange,
     turmaId,
     questionarioId,
+    versaoPlano,
   } = props;
 
   const [lista, setLista] = useState([]);
@@ -54,7 +55,10 @@ const CampoDinamicoPeriodoEscolar = props => {
     )
       .catch(e => erros(e))
       .finally(() => setExibirLoader(false));
-    if (retorno?.data) {
+      const anoAtual = new Date().getFullYear();
+      const anoVersao = versaoPlano? new Date(versaoPlano.criadoEm).getFullYear() : null ;
+
+    if (retorno?.data && anoVersao === anoAtual) {
       habilitaEdicaoSeMudarBimestreAtual(questaoAtual, retorno.data.id);
       form.setFieldValue(String(questaoAtual.id), String(retorno.data.id));
     } else {
