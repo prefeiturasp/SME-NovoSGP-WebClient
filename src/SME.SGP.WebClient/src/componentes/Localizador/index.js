be811efca8f94933fe75dc378652237635d18c0c
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2022-12-19 14:41:21 -0300

    Localizador - add nova prop novaEstrutura

--- src/SME.SGP.WebClient/src/componentes/Localizador/index.js
+++ src/SME.SGP.WebClient/src/componentes/Localizador/index.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
 // Componentes
+import { Col } from 'antd';
 import InputRF from './componentes/InputRF';
 import InputNome from './componentes/InputNome';
 import { Grid, Label } from '~/componentes';
@@ -44,6 +45,7 @@ function Localizador({
   buscarPorAbrangencia,
   labelRequired,
   buscarPorTodasDre,
+  novaEstrutura,
 }) {
   const usuario = useSelector(store => store.usuario);
   const [dataSource, setDataSource] = useState([]);
@@ -234,6 +236,11 @@ function Localizador({
           professorNome: '',
           usuarioId: '',
         });
+        setDesabilitarCampo({
+          rf: false,
+          nome: false,
+        });
+        setDataSource([]);
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -248,6 +255,67 @@ function Localizador({
       });
     }
   }, [pessoaSelecionada, limparCamposAposPesquisa]);
+
+  if (novaEstrutura) {
+    return (
+      <>
+        <Col sm={24} md={8}>
+          {showLabel && (
+            <Label
+              text={labelRF}
+              control="professorRf"
+              isRequired={labelRequired}
+            />
+          )}
+          <InputRF
+            id={SGP_INPUT_RF}
+            pessoaSelecionada={pessoaSelecionada}
+            onSelect={onBuscarPorRF}
+            onChange={v => {
+              if (v?.length === 0) {
+                setDataSource([]);
+              }
+              onChangeRF(v);
+            }}
+            name="professorRf"
+            placeholderRF={placeholderRF}
+            form={form}
+            desabilitado={
+              desabilitado ||
+              validacaoDesabilitaPerfilProfessor() ||
+              desabilitarCampo.rf
+            }
+            exibirLoader={exibirLoader}
+          />
+        </Col>
+        <Col sm={24} md={16}>
+          {showLabel && (
+            <Label
+              text={labelNome}
+              control="professorNome"
+              isRequired={labelRequired}
+            />
+          )}
+          <InputNome
+            id={SGP_INPUT_NOME}
+            dataSource={dataSource}
+            onSelect={onSelectPessoa}
+            onChange={onChangeInput}
+            pessoaSelecionada={pessoaSelecionada}
+            form={form}
+            name="professorNome"
+            placeholderNome={placeholderNome}
+            desabilitado={
+              desabilitado ||
+              validacaoDesabilitaPerfilProfessor() ||
+              desabilitarCampo.nome
+            }
+            exibirLoader={exibirLoader}
+          />
+        </Col>
+      </>
+    );
+  }
 
   return (
     <>
@@ -331,6 +399,7 @@ Localizador.propTypes = {
   buscarPorAbrangencia: PropTypes.bool,
   labelRequired: PropTypes.bool,
   buscarPorTodasDre: PropTypes.bool,
+  novaEstrutura: PropTypes.bool,
 };
 
 Localizador.defaultProps = {
@@ -357,6 +426,7 @@ Localizador.defaultProps = {
   buscarPorAbrangencia: false,
   labelRequired: false,
   buscarPorTodasDre: false,
+  novaEstrutura: false,
 };
 
 export default Localizador;
