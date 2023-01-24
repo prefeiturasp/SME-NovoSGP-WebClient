c5b5a71de36195da9d096cd119f1d7149f50e22b
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2023-01-24 17:59:24 -0300

    Listão Fechamento - melhorias no layout
    add ícone de ausente e nome dos componentes ao expandir detalhes das avaliações, ajustado layout e criado nova usabilidade para regente de nota e conceito

--- src/SME.SGP.WebClient/src/paginas/DiarioClasse/Listao/operacoes/listaoTabs/tabFechamento/listaoListaFechamento.js
+++ src/SME.SGP.WebClient/src/paginas/DiarioClasse/Listao/operacoes/listaoTabs/tabFechamento/listaoListaFechamento.js
@@ -11,7 +11,7 @@ import React, { useContext, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import shortid from 'shortid';
 import styled from 'styled-components';
-import { DataTable } from '~/componentes';
+import { DataTable, Label } from '~/componentes';
 import Nota from '~/componentes-sgp/inputs/nota';
 import { moverFocoCampoNota } from '~/componentes-sgp/inputs/nota/funcoes';
 import SinalizacaoAEE from '~/componentes-sgp/SinalizacaoAEE/sinalizacaoAEE';
@@ -50,6 +50,43 @@ export const ContainerTableFechamento = styled.div`
         overflow: auto !important;
       }
     }
+
+    .padding-componentes-regencia {
+      td {
+        padding: 2px !important;
+      }
+    }
+
+    .icone-inicial-linha-expandida {
+      tbody {
+        th {
+          background: ${Base.RoxoBorda} !important;
+        }
+
+        &::after {
+          content: ' ';
+          position: absolute;
+          font-family: 'Font Awesome 5 Free';
+          font-weight: 900;
+          border: 1px solid ${Base.Roxo};
+          height: 27px;
+          left: -24px;
+          top: -2px;
+          white-space: nowrap;
+        }
+
+        &::before {
+          content: '\f30b';
+          font-family: 'Font Awesome 5 Free';
+          font-weight: 900;
+          font-size: 16px;
+          color: ${Base.Roxo};
+          position: absolute;
+          left: -23px;
+          top: 12px;
+        }
+      }
+    }
   }
 `;
 
@@ -66,7 +103,9 @@ const ListaoListaFechamento = props => {
     listaoEhInfantil,
   } = useContext(ListaoContext);
 
-  const [expandedRowKeys, setExpandedRowKeys] = useState();
+  const [expandedRowKeys, setExpandedRowKeys] = useState(
+    dadosFechamento?.alunos?.map(a => ({ codigoAluno: a?.codigoAluno }))
+  );
 
   const { ehEJA } = props;
 
@@ -94,11 +133,20 @@ const ListaoListaFechamento = props => {
     </span>
   );
 
-  const temLinhaExpandida = codigoAluno =>
-    expandedRowKeys?.codigoAluno === codigoAluno;
+  const temLinhaAvaliacoesExpandida = codigoAluno =>
+    !!expandedRowKeys?.find?.(
+      r => r?.codigoAluno === codigoAluno && !!r?.expandirAvaliacoes
+    );
+
+  const temLinhaNotaConceitoExpandida = codigoAluno =>
+    !!expandedRowKeys?.find?.(
+      r => r?.codigoAluno === codigoAluno && !r?.ocultarLinha
+    );
 
   const montarColunaEstudante = aluno => {
-    const alunoExpandido = temLinhaExpandida(aluno?.codigoAluno);
+    const alunoExpandido =
+      temLinhaAvaliacoesExpandida(aluno?.codigoAluno) ||
+      temLinhaNotaConceitoExpandida(aluno?.codigoAluno);
 
     return (
       <div className="d-flex justify-content-between">
@@ -158,14 +206,53 @@ const ListaoListaFechamento = props => {
     }
   };
 
-  const onClickExpandir = (expandir, codigoAluno, expandirColunaRegencia) => {
-    if (expandir) {
-      setExpandedRowKeys({
-        codigoAluno,
-        expandirColunaRegencia,
-      });
+  const onClickExpandirAvaliacoes = (expandirAvaliacoes, codigoAluno) => {
+    let index = 0;
+    let alunoExpandido = null;
+
+    if (expandedRowKeys?.length) {
+      alunoExpandido = expandedRowKeys?.find(
+        r => r.codigoAluno?.toString() === codigoAluno?.toString()
+      );
+      index = expandedRowKeys.indexOf(alunoExpandido);
+
+      if (index > -1) {
+        const rowKeys = [...expandedRowKeys];
+        rowKeys[index].expandirAvaliacoes = expandirAvaliacoes;
+        setExpandedRowKeys(rowKeys);
+      }
     } else {
-      setExpandedRowKeys();
+      setExpandedRowKeys([
+        {
+          codigoAluno,
+          expandirAvaliacoes,
+        },
+      ]);
+    }
+  };
+
+  const onClickExpandirNotaConceitoRegencia = (ocultarLinha, codigoAluno) => {
+    let index = 0;
+    let alunoExpandido = null;
+
+    if (expandedRowKeys?.length) {
+      alunoExpandido = expandedRowKeys?.find(
+        r => r.codigoAluno?.toString() === codigoAluno?.toString()
+      );
+      index = expandedRowKeys.indexOf(alunoExpandido);
+
+      if (index > -1) {
+        const rowKeys = [...expandedRowKeys];
+        rowKeys[index].ocultarLinha = ocultarLinha;
+        setExpandedRowKeys(rowKeys);
+      }
+    } else {
+      setExpandedRowKeys([
+        {
+          codigoAluno,
+          ocultarLinha,
+        },
+      ]);
     }
   };
 
@@ -176,7 +263,7 @@ const ListaoListaFechamento = props => {
     const indexLinhaDestino = incrementoIndexLinha + indexLinhaOrigem;
     const aluno = dadosFechamento?.alunos[indexLinhaDestino];
     if (aluno?.codigoAluno) {
-      onClickExpandir(true, aluno?.codigoAluno, true);
+      onClickExpandirNotaConceitoRegencia(false, aluno?.codigoAluno);
     }
   };
 
@@ -227,7 +314,15 @@ const ListaoListaFechamento = props => {
       case notasConceitos.Notas:
         return (
           <div name={idDisciplina} id={idDisciplina}>
+            {ehRegencia && (
+              <Label
+                text={notaFechamento?.disciplina}
+                tamanhoFonte="11"
+                altura="0"
+              />
+            )}
             <Nota
+              styleContainer={{ padding: '3px 20px 11px' }}
               ehFechamento
               onKeyDown={e =>
                 onKeyDown(
@@ -259,7 +354,15 @@ const ListaoListaFechamento = props => {
       case notasConceitos.Conceitos:
         return (
           <>
+            {ehRegencia && (
+              <Label
+                text={notaFechamento?.disciplina}
+                tamanhoFonte="11"
+                altura="0"
+              />
+            )}
             <ListaoCampoConceito
+              styleContainer={{ padding: '3px 20px 11px' }}
               dadosConceito={notaFechamento}
               idCampo={shortid.generate()}
               desabilitar={desabilitar}
@@ -319,11 +422,14 @@ const ListaoListaFechamento = props => {
       style={{
         fontSize: 18,
         cursor: 'pointer',
-        color: alunoExpandido ? Base.Branco : Base.Roxo,
+        color: Base.Roxo,
       }}
       icon={alunoExpandido ? faMinusCircle : faPlusCircle}
       onClick={() =>
-        onClickExpandir(!alunoExpandido, dadosEstudante?.codigoAluno, true)
+        onClickExpandirNotaConceitoRegencia(
+          alunoExpandido,
+          dadosEstudante?.codigoAluno
+        )
       }
     />
   );
@@ -348,7 +454,7 @@ const ListaoListaFechamento = props => {
           ? 'Nota bimestre'
           : 'Conceito bimestre',
       align: 'center',
-      width: '110px',
+      width: '115px',
       children: montarColunaNotaConceitoPorBimestre(),
     });
 
@@ -358,7 +464,7 @@ const ListaoListaFechamento = props => {
           ? 'Nota final'
           : 'Conceito final',
       align: 'center',
-      width: '110px',
+      width: '115px',
     };
 
     if (ehRegencia) {
@@ -367,7 +473,9 @@ const ListaoListaFechamento = props => {
           item => item?.emAprovacao
         );
 
-        const alunoExpandido = temLinhaExpandida(dadosEstudante?.codigoAluno);
+        const alunoExpandido = temLinhaNotaConceitoExpandida(
+          dadosEstudante?.codigoAluno
+        );
         const iconeExpandir = iconeExpandirLinhaRegencia(
           alunoExpandido,
           dadosEstudante
@@ -407,7 +515,8 @@ const ListaoListaFechamento = props => {
           ? 'Nota'
           : 'Conceito',
       align: 'center',
-      width: '110px',
+      width: '115px',
+      className: 'position-relative',
     };
 
     if (ehRegencia) {
@@ -416,7 +525,9 @@ const ListaoListaFechamento = props => {
           item => item?.emAprovacao
         );
 
-        const alunoExpandido = temLinhaExpandida(dadosEstudante?.codigoAluno);
+        const alunoExpandido = temLinhaNotaConceitoExpandida(
+          dadosEstudante?.codigoAluno
+        );
         const iconeExpandir = iconeExpandirLinhaRegencia(
           alunoExpandido,
           dadosEstudante
@@ -456,7 +567,7 @@ const ListaoListaFechamento = props => {
       title: 'Frequência',
       align: 'center',
       dataIndex: 'frequencia',
-      width: '110px',
+      width: '115px',
       render: percentualFrequencia =>
         percentualFrequencia ? `${percentualFrequencia}%` : '',
     });
@@ -464,18 +575,23 @@ const ListaoListaFechamento = props => {
 
   if (dadosFechamento?.possuiAvaliacao) {
     colunasEstudantes.push({
-      title: 'Informações adicionais',
+      title: 'Avaliações',
       align: 'center',
       width: '115px',
       render: dadosEstudante => {
-        const alunoExpandido = temLinhaExpandida(dadosEstudante?.codigoAluno);
+        const alunoExpandido = temLinhaAvaliacoesExpandida(
+          dadosEstudante?.codigoAluno
+        );
         return (
           // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
           <div
             className="d-flex justify-content-center align-items-center"
             style={{ cursor: 'pointer' }}
             onClick={() =>
-              onClickExpandir(!alunoExpandido, dadosEstudante?.codigoAluno)
+              onClickExpandirAvaliacoes(
+                !alunoExpandido,
+                dadosEstudante?.codigoAluno
+              )
             }
           >
             <div style={{ marginRight: '4px' }}>Detalhar</div>
@@ -483,7 +599,7 @@ const ListaoListaFechamento = props => {
               style={{
                 fontSize: 16,
                 cursor: 'pointer',
-                color: alunoExpandido ? Base.Branco : Base.CinzaMako,
+                color: Base.CinzaMako,
               }}
               icon={alunoExpandido ? faChevronUp : faChevronDown}
             />
@@ -510,7 +626,8 @@ const ListaoListaFechamento = props => {
           align: 'center',
           dataIndex: `${nomeRef}[${index}]`,
           key: `${nomeRef}[${index}]`,
-          width: '110px',
+          width: '115px',
+          className: 'position-relative',
           render: dados =>
             montarCampoNotaConceito(
               estudante,
@@ -547,6 +664,16 @@ const ListaoListaFechamento = props => {
     </div>
   );
 
+  const getExpandedRowKeys = () => {
+    if (expandedRowKeys?.length) {
+      const alunosExpandidos = expandedRowKeys.filter(
+        r => !r?.ocultarLinha || !!r.expandirAvaliacoes
+      );
+      return alunosExpandidos.map(a => a.codigoAluno);
+    }
+    return [];
+  };
+
   const montarTabelaRegencia = () => (
     <>
       {!ehFinal && (
@@ -571,43 +698,47 @@ const ListaoListaFechamento = props => {
           tableResponsive={false}
           idLinha="codigoAluno"
           expandIconColumnIndex={getExpandIconColumnIndex()}
-          expandedRowKeys={
-            expandedRowKeys?.codigoAluno ? [expandedRowKeys.codigoAluno] : []
-          }
-          rowClassName={record => {
-            const alunoExpandido = temLinhaExpandida(record?.codigoAluno);
-            const nomeClasse = alunoExpandido ? 'linha-ativa' : '';
-            return nomeClasse;
-          }}
+          expandedRowKeys={getExpandedRowKeys()}
           expandedRowRender={(record, indexAluno) => {
             const colunasDetalhe = montarColunasNotasConceitosRegencia(
               record,
               indexAluno
             );
-
-            if (expandedRowKeys?.expandirColunaRegencia) {
-              return (
-                <DataTable
-                  id={`tabela-aluno-${record?.codigoAluno}`}
-                  idLinha="codigoAluno"
-                  pagination={false}
-                  columns={colunasDetalhe}
-                  dataSource={[record]}
-                  semHover
-                />
-              );
-            }
+            const linhaAvaliacaoExpandida = temLinhaAvaliacoesExpandida(
+              record?.codigoAluno
+            );
+            const linhaNotaConceitoExpandida = temLinhaNotaConceitoExpandida(
+              record?.codigoAluno
+            );
 
             return (
-              <TabelaAvaliacoesFechamento
-                codigoAluno={record?.codigoAluno}
-                periodoEscolarId={dadosFechamento?.periodoEscolarId}
-                ehNota={
-                  Number(dadosFechamento?.notaTipo) === notasConceitos.Notas
-                }
-                listaTiposConceitos={dadosFechamento?.listaTiposConceitos}
-                componenteCurricular={componenteCurricular}
-              />
+              <>
+                {linhaNotaConceitoExpandida && (
+                  <div className="icone-inicial-linha-expandida padding-componentes-regencia">
+                    <DataTable
+                      showHeader={false}
+                      id={`tabela-aluno-${record?.codigoAluno}`}
+                      idLinha="codigoAluno"
+                      pagination={false}
+                      columns={colunasDetalhe}
+                      dataSource={[record]}
+                      semHover
+                      rowClassName="linha-expandida-regencia-listao-fechamento"
+                    />
+                  </div>
+                )}
+                {linhaAvaliacaoExpandida && (
+                  <TabelaAvaliacoesFechamento
+                    codigoAluno={record?.codigoAluno}
+                    periodoEscolarId={dadosFechamento?.periodoEscolarId}
+                    ehNota={
+                      Number(dadosFechamento?.notaTipo) === notasConceitos.Notas
+                    }
+                    listaTiposConceitos={dadosFechamento?.listaTiposConceitos}
+                    componenteCurricular={componenteCurricular}
+                  />
+                )}
+              </>
             );
           }}
           expandIcon={() => ''}
@@ -627,18 +758,17 @@ const ListaoListaFechamento = props => {
       <LinhaTabela className="col-md-12 p-0">
         <DataTable
           fixExpandedRowResetColSpan
-          scroll={{ x: 1000, y: 500 }}
           columns={colunasEstudantes}
           dataSource={dadosFechamento?.alunos}
           pagination={false}
           semHover
           idLinha="codigoAluno"
           expandIconColumnIndex={getExpandIconColumnIndex()}
-          expandedRowKeys={
-            expandedRowKeys?.codigoAluno ? [expandedRowKeys.codigoAluno] : []
-          }
+          expandedRowKeys={getExpandedRowKeys()}
           rowClassName={record => {
-            const alunoExpandido = temLinhaExpandida(record?.codigoAluno);
+            const alunoExpandido =
+              temLinhaAvaliacoesExpandida(record?.codigoAluno) ||
+              temLinhaNotaConceitoExpandida(record?.codigoAluno);
             const nomeClasse = alunoExpandido ? 'linha-ativa' : '';
             return nomeClasse;
           }}
