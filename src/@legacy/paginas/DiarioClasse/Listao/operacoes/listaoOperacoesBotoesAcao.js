79dc75508432267de2210f310e5a27f284d56fe2
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2023-03-21 15:01:27 -0300

    Frequência Listão - exibir datas de aulas com erros ao salvar

--- src/@legacy/paginas/DiarioClasse/Listao/operacoes/listaoOperacoesBotoesAcao.js
+++ src/@legacy/paginas/DiarioClasse/Listao/operacoes/listaoOperacoesBotoesAcao.js
@@ -144,13 +144,64 @@ const ListaoOperacoesBotoesAcao = () => {
       .finally(() => setExibirLoaderGeral(false));
 
     if (resposta?.data) {
-      const auditoriaNova = resposta.data;
-      dadosFrequencia.auditoria = { ...auditoriaNova };
-      dadosIniciaisFrequencia.auditoria = { ...auditoriaNova };
-      setDadosFrequencia({ ...dadosFrequencia });
-      setDadosIniciaisFrequencia(dadosIniciaisFrequencia);
+      const auditoriaNova = resposta.data?.auditoria;
+      if (auditoriaNova?.id) {
+        dadosFrequencia.auditoria = { ...auditoriaNova };
+        dadosIniciaisFrequencia.auditoria = { ...auditoriaNova };
+        setDadosFrequencia({ ...dadosFrequencia });
+        setDadosIniciaisFrequencia(dadosIniciaisFrequencia);
+      }
 
-      sucesso('Frequência realizada com sucesso.');
+      const aulasIDsComErros = resposta.data?.aulasIDsComErros;
+
+      if (aulasIDsComErros?.length) {
+        const aulasComErros = [];
+        const aulasSemErros = [];
+
+        dadosFrequencia.aulas.forEach(aula => {
+          const aulaComErro = aulasIDsComErros.find(
+            aulaIdErro => aulaIdErro === aula?.aulaId
+          );
+          if (aulaComErro) {
+            aulasComErros.push(aula);
+          } else {
+            aulasSemErros.push(aula);
+          }
+        });
+
+        if (aulasComErros?.length) {
+          const dataAulasComErros = aulasComErros
+            .map(a => a?.dataAula)
+            .join(', ');
+
+          let mensagemErro = 'Não foi possível registrar a frequência';
+          if (aulasComErros?.length > 1) {
+            mensagemErro = `${mensagemErro} dos dias ${dataAulasComErros}`;
+          } else {
+            mensagemErro = `${mensagemErro} do dia ${dataAulasComErros}`;
+          }
+
+          erro(mensagemErro);
+        }
+
+        if (aulasSemErros?.length) {
+          const dataAulasSemErros = aulasSemErros
+            .map(a => a?.dataAula)
+            .join(', ');
+
+          let mensagemSucesso = 'Frequência realizada com sucesso';
+          if (aulasSemErros?.length > 1) {
+            mensagemSucesso = `${mensagemSucesso} dos dias ${dataAulasSemErros}`;
+          } else {
+            mensagemSucesso = `${mensagemSucesso} do dia ${dataAulasSemErros}`;
+          }
+
+          sucesso(mensagemSucesso);
+        }
+      } else {
+        sucesso('Frequência realizada com sucesso.');
+      }
+
       dispatch(setTelaEmEdicao(false));
       return true;
     }
@@ -403,9 +454,8 @@ const ListaoOperacoesBotoesAcao = () => {
       return true;
     }
 
-    const errosDiarioBordo = validarCamposObrigatoriosDiarioBordo(
-      dadosAlterados
-    );
+    const errosDiarioBordo =
+      validarCamposObrigatoriosDiarioBordo(dadosAlterados);
 
     if (errosDiarioBordo?.length) {
       setErrosDiarioBordoListao(errosDiarioBordo);
@@ -587,7 +637,6 @@ const ListaoOperacoesBotoesAcao = () => {
     } else {
       dispatch(setLimparModoEdicaoGeral());
     }
-
   }, [telaEmEdicao]);
 
   const onClickVoltar = async () => {
