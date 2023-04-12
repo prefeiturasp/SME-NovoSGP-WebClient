2ca2f6c95cbbf456a5ad3c478d0ddba5290eb499
Guilherme Rocha <guimacielr@gmail.com>
2023-04-12 12:01:39 -0300

    Ajuste para que as tabs não comecem selecionadas

--- src/@legacy/paginas/Dashboard/DashboardFechamento/TabsDashboardFechamento/tabsDashboardFechamento.js
+++ src/@legacy/paginas/Dashboard/DashboardFechamento/TabsDashboardFechamento/tabsDashboardFechamento.js
@@ -11,7 +11,7 @@ import { ContainerTabsDashboardFechamento } from '../dashboardFechamento.css';
 import { ContainerTabsCard } from '~/componentes/tabs/tabs.css';
 
 const TabsDashboardFechamento = () => {
-  const [tabSelecionada, setTabSelecionada] = useState();
+  const [tabSelecionada, setTabSelecionada] = useState(0);
   const anoLetivo = useSelector(
     store => store.dashboardFechamento?.dadosDashboardFechamento?.anoLetivo
   );
