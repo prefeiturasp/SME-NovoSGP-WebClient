d27f6beca5defda9ac7766651a19bf4b799d8e83
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2023-04-14 12:55:50 -0300

    Fix CSS lista transferência

--- src/@legacy/estilos/global.js
+++ src/@legacy/estilos/global.js
@@ -72,7 +72,7 @@ export default createGlobalStyle`
     color: #ffffff !important;
   }
 
-  /* Antd V5 */
+  /* Antd V5 Start */
   .ant-select {
     width: 100%;
   }
@@ -173,7 +173,11 @@ export default createGlobalStyle`
     color: #ffffff !important;
   }
 
-  /* Antd V5 */
+  .ant-table-thead > tr:hover:not(.ant-table-expanded-row):not(.ant-table-row-selected) > td.ant-table-cell-scrollbar {
+    background: ${Base.CinzaTabela} !important;
+  }
+
+  /* Antd V5 End */
 
   .ant-select-dropdown-menu-item  {
     -webkit-transition: none !important;
