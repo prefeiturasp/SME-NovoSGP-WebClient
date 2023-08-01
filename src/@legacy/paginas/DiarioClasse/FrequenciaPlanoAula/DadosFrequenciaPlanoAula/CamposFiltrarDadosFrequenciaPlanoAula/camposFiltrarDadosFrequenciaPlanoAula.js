486688ba0d096089a2bf7a710fc521c933d94e7b
Helio Barros <helio.barros@amcom.com.br>
2023-08-01 11:59:57 -0300

    AB#100603 - ajuste para atender todas modalidades

--- src/@legacy/paginas/DiarioClasse/FrequenciaPlanoAula/DadosFrequenciaPlanoAula/CamposFiltrarDadosFrequenciaPlanoAula/camposFiltrarDadosFrequenciaPlanoAula.js
+++ src/@legacy/paginas/DiarioClasse/FrequenciaPlanoAula/DadosFrequenciaPlanoAula/CamposFiltrarDadosFrequenciaPlanoAula/camposFiltrarDadosFrequenciaPlanoAula.js
@@ -407,7 +407,7 @@ const CamposFiltrarDadosFrequenciaPlanoAula = () => {
           id={SGP_SELECT_COMPONENTE_CURRICULAR}
           lista={listaComponenteCurricular}
           valueOption="id"
-          valueText="nomeComponenteInfantil"
+          valueText="nome"
           valueSelect={codigoComponenteCurricular}
           onChange={onChangeComponenteCurricular}
           placeholder="Selecione um componente curricular"
