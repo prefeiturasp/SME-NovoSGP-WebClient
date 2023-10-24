fa4e1431b651045a55826aaa1245e1db94b6721e
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2023-10-24 03:22:18 -0300

    fix Date.prototype.toISOString

--- src/core/config/bibliotecas/moment.ts
+++ src/core/config/bibliotecas/moment.ts
@@ -4,6 +4,6 @@ moment.locale('pt-br');
 window.moment = moment;
 
 // eslint-disable-next-line no-extend-native, func-names
-Date.prototype.toISOString = () => {
+Date.prototype.toISOString = function () {
   return moment(this).format('YYYY-MM-DDTHH:mm:ss');
 };
