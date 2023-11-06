b4d3e1b560294516d31403a072973c66dbb88017
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2023-11-06 10:02:31 -0300

    fix prettier build

--- src/@legacy/paginas/CalendarioEscolar/TipoCalendarioEscolar/tipoCalendarioEscolarForm.js
+++ src/@legacy/paginas/CalendarioEscolar/TipoCalendarioEscolar/tipoCalendarioEscolarForm.js
@@ -376,7 +376,7 @@ const TipoCalendarioEscolarForm = () => {
   );
 
   useEffect(() => {
-    if(!idTipoCalendario){
+    if (!idTipoCalendario) {
       obterAnosLetivos();
     }
     verificarSeSemestreEhRequerido();
@@ -446,7 +446,8 @@ const TipoCalendarioEscolarForm = () => {
                         valueOption="valor"
                         valueText="desc"
                         disabled={
-                          (idTipoCalendario > 0) || listaAnosLetivo && listaAnosLetivo.length === 1
+                          idTipoCalendario > 0 ||
+                          (listaAnosLetivo && listaAnosLetivo.length === 1)
                         }
                         onChange={onChangeAnoLetivo}
                         valueSelect={anoLetivo}
