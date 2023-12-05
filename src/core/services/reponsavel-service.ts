2a339ce9c6bd993554a2269f1d659aa706605572
DanylloPereira <danyllo.antunes@gmail.com>
2023-12-05 16:59:20 -0300

    105446 - Alteração de nomeclatura endpoint

--- src/core/services/reponsavel-service.ts
+++ src/core/services/reponsavel-service.ts
@@ -4,7 +4,7 @@ import { alterarRegistro } from './api';
 const URL_DEFAULT = 'v1/busca-ativa';
 
 const atualizarDadosResponsavel = (params: DadosResponsavelAtualizarDto) =>
-  alterarRegistro<boolean>(`${URL_DEFAULT}/atualizar/dados-usuario`, params);
+  alterarRegistro<boolean>(`${URL_DEFAULT}/criancas-estudantes/responsaveis`, params);
 
 export default {
   atualizarDadosResponsavel,
