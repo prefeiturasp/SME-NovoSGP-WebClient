76bd6ff655aae96dbc250c1a883c38ecec1610f0
Igor Hebling Sallowicz <igor.sallo@hotmail.com>
2023-12-11 19:39:41 -0300

    Removendo google fotos

--- src/@legacy/componentes/jodit-editor/joditEditor.js
+++ src/@legacy/componentes/jodit-editor/joditEditor.js
@@ -70,7 +70,8 @@ const converterImagemURLExternaParaInterna = async urlExterna => {
   const localFile =
     urlExterna?.startsWith('file:///') ||
     urlExterna?.startsWith('blob:https://web.whatsapp.com/') ||
-    urlExterna?.startsWith(' https://attachment.outlook.live.net');
+    urlExterna?.startsWith(' https://attachment.outlook.live.net') ||
+    urlExterna?.startsWith('https://lh3.googleusercontent.com/');
 
   if (localFile) return urlExterna;
 
