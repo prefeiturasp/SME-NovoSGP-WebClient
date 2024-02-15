7ba89c1c8425f43afe7e3f6135a4ae65506d64fb
DanylloPereira <danyllo.antunes@gmail.com>
2024-02-15 11:36:47 -0300

    96713 - Ajuste back e front

--- src/core/services/registro-coletivo-service.ts
+++ src/core/services/registro-coletivo-service.ts
@@ -2,7 +2,7 @@ import { URL_API_REGISTRO_COLETIVO } from '../constants/urls-api';
 import { RegistroColetivoCompletoDto } from '../dto/RegistroColetivoCompletoDto';
 import { RegistroColetivoDto } from '../dto/RegistroColetivoDto';
 import { TipoReuniaoDto } from '../dto/TipoReuniaoDto';
-import { alterarRegistro, deletarRegistro, inserirRegistro, obterRegistro } from './api';
+import { deletarRegistro, inserirRegistro, obterRegistro } from './api';
 
 const obterTipoDeReuniaoNAAPA = () =>
   obterRegistro<TipoReuniaoDto[]>(`${URL_API_REGISTRO_COLETIVO}/tipo-reuniao`);
@@ -11,7 +11,7 @@ const salvar = (params: RegistroColetivoDto) =>
   inserirRegistro(`${URL_API_REGISTRO_COLETIVO}/salvar`, params);
 
 const alterar = (params: RegistroColetivoDto) =>
-  alterarRegistro(`${URL_API_REGISTRO_COLETIVO}/salvar`, params);
+  inserirRegistro(`${URL_API_REGISTRO_COLETIVO}/salvar`, params);
 
 const buscarPorId = (registroColetivoId: number | string) =>
   obterRegistro<RegistroColetivoCompletoDto>(`${URL_API_REGISTRO_COLETIVO}/${registroColetivoId}`);
