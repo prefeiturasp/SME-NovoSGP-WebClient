78cb15c1d0ded1bdc1e24a3a055da0946b0e48aa
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2024-03-01 12:00:40 -0300

    desabilitar select parecer quando selecionado o parecer atual

--- src/@legacy/paginas/Fechamento/ConselhoClasse/DadosConselhoClasse/MarcadorParecerConclusivo/marcadorParecerConclusivo.js
+++ src/@legacy/paginas/Fechamento/ConselhoClasse/DadosConselhoClasse/MarcadorParecerConclusivo/marcadorParecerConclusivo.js
@@ -156,6 +156,7 @@ const MarcadorParecerConclusivo = () => {
             turmaId={turmaSelecionada?.id}
             onChange={onChangeParecer}
             value={parecerSelecionado}
+            disabled={!!marcadorParecerConclusivo?.id && marcadorParecerConclusivo.id === parecerSelecionado}
           />
         </Col>
         <Col>
