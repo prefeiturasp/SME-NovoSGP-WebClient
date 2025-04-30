02868ff92fa29a7a6f866b89ed494cff865bb957
Max Fernandes de Souza <max.souza@spassu.com.br>
2025-04-30 14:32:44 -0300

    Correção do componente de disciplinas do Plano Anual

--- src/@legacy/paginas/Planejamento/Anual/DadosPlanoAnual/ComponenteCurricular/componenteCurricularPlanoAnual.js
+++ src/@legacy/paginas/Planejamento/Anual/DadosPlanoAnual/ComponenteCurricular/componenteCurricularPlanoAnual.js
@@ -5,7 +5,7 @@ import {
   limparDadosPlanoAnual,
   setComponenteCurricularPlanoAnual,
 } from '~/redux/modulos/anual/actions';
-import { confirmar, erros, aviso } from '~/servicos/alertas';
+import { confirmar, erros } from '~/servicos/alertas';
 import ServicoDisciplinas from '~/servicos/Paginas/ServicoDisciplina';
 import { ehTurmaInfantil } from '~/servicos/Validacoes/validacoesInfatil';
 import servicoSalvarPlanoAnual from '../../servicoSalvarPlanoAnual';
@@ -35,9 +35,6 @@ const ComponenteCurricularPlanoAnual = () => {
   const [codigoComponenteCurricular, setCodigoComponenteCurricular] =
     useState(undefined);
 
-  const avisoPlanoTerritorio =
-    'O plano anual do território do saber deve ser registrado na tela "Planejamento > Território do saber';
-
   const obterListaComponenteCurricular = useCallback(async () => {
     setCarregandoComponentes(true);
     const resposta = await ServicoDisciplinas.obterDisciplinasPorTurma(
@@ -47,15 +44,8 @@ const ComponenteCurricularPlanoAnual = () => {
       setCarregandoComponentes(false);
     });
     if (resposta && resposta.data) {
-      setListaComponenteCurricular(resposta.data);
-      if (resposta.data.length === 1) {
-        const componente = resposta.data[0];
-        if (componente.territorioSaber) {
-          dispatch(limparDadosPlanoAnual());
-          dispatch(setComponenteCurricularPlanoAnual(undefined));
-          aviso(avisoPlanoTerritorio);
-        } else dispatch(setComponenteCurricularPlanoAnual(componente));
-      }
+      const lista = resposta.data.filter(d => !d.territorioSaber);
+      setListaComponenteCurricular(lista);
     } else {
       setListaComponenteCurricular([]);
       dispatch(setComponenteCurricularPlanoAnual(undefined));
@@ -103,11 +93,8 @@ const ComponenteCurricularPlanoAnual = () => {
         const componente = listaComponenteCurricular.find(
           item => String(item.codigoComponenteCurricular) === valor
         );
-        if (componente.territorioSaber) aviso(avisoPlanoTerritorio);
-        else {
-          dispatch(limparDadosPlanoAnual());
-          dispatch(setComponenteCurricularPlanoAnual(componente));
-        }
+        dispatch(limparDadosPlanoAnual());
+        dispatch(setComponenteCurricularPlanoAnual(componente));
       } else {
         dispatch(limparDadosPlanoAnual());
         dispatch(setComponenteCurricularPlanoAnual(undefined));
