29823301df249ab89805aa0ba69a9ce6c7d00972
Max Fernandes <xaamax@gmail.com>
2025-09-30 10:50:30 -0300

    Ajuste no modo de edição do componente de anotação em Frequencia/Plano Aula

--- src/@legacy/componentes-sgp/ModalAnotacoes/modalAnotacoes.js
+++ src/@legacy/componentes-sgp/ModalAnotacoes/modalAnotacoes.js
@@ -12,6 +12,10 @@ import SelectComponent from '~/componentes/select';
 import { confirmar, erros, sucesso } from '~/servicos/alertas';
 import ServicoAnotacaoFrequenciaAluno from '~/servicos/Paginas/DiarioClasse/ServicoAnotacaoFrequenciaAluno';
 import { EditorAnotacao } from './modalAnotacoes.css';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import { Typography } from 'antd';
+const { Text } = Typography;
 
 const ModalAnotacoesFrequencia = props => {
   const {
@@ -27,6 +31,7 @@ const ModalAnotacoesFrequencia = props => {
     fechouModal,
     listaPadraoMotivoAusencia,
     exibirMotivosAusencia = true,
+    dataSelecionadaMotivosAusencias,
   } = props;
 
   const dispatch = useDispatch();
@@ -55,6 +60,7 @@ const ModalAnotacoesFrequencia = props => {
   const [refForm, setRefForm] = useState({});
   const [valoresIniciais, setValoresIniciais] = useState(iniciar);
   const [loaderSalvarEditar, setLoaderSalvarEditar] = useState(false);
+  const [motivosAusenciaAbae, setMotivosAusenciaAbae] = useState([]);
   const exibirEditor = useRef(false);
 
   const validacoes = Yup.object().shape(
@@ -110,6 +116,24 @@ const ModalAnotacoesFrequencia = props => {
     }
   }, [aulaId, dadosModal]);
 
+  const obterMotivosAusenciasAbae = useCallback(async () => {
+    const resultado =
+      await ServicoAnotacaoFrequenciaAluno.obterMotivosAusenciaAbae(
+        dadosModal?.codigoAluno,
+        moment(dataSelecionadaMotivosAusencias).format('YYYY-MM-DD HH:mm:ss')
+      ).catch(e => erros(e));
+
+    if (resultado && resultado.data) {
+      const registrosFiltrados = resultado.data.items.filter(
+        item => item.descMotivoAusencia
+      );
+
+      setMotivosAusenciaAbae(registrosFiltrados);
+    } else {
+      setMotivosAusenciaAbae({});
+    }
+  }, []);
+
   const obterListaMotivosAusencia = async () => {
     const retorno =
       await ServicoAnotacaoFrequenciaAluno.obterMotivosAusencia().catch(e =>
@@ -151,6 +175,7 @@ const ModalAnotacoesFrequencia = props => {
     if (dadosModal?.codigoAluno) {
       obterAnotacao();
       montarDadosAluno();
+      obterMotivosAusenciasAbae();
     }
   }, [dadosModal, obterAnotacao, montarDadosAluno]);
 
@@ -318,6 +343,22 @@ const ModalAnotacoesFrequencia = props => {
                   permiteAlterarImagem={false}
                 />
               </div>
+
+              {motivosAusenciaAbae.length > 0 ? (
+                <div className="col-md-12 mt-2">
+                  {' '}
+                  <strong>Motivos de ausência cadastrado pelo ABAE</strong>
+                </div>
+              ) : (
+                ''
+              )}
+              {motivosAusenciaAbae.map(item => (
+                <div className="col-md-12 mt-2" key={item.id}>
+                  {' '}
+                  <Text>{item.descMotivoAusencia}</Text>
+                </div>
+              ))}
+
               {exibirMotivosAusencia ? (
                 <div className="col-md-12 mt-2">
                   <Loader loading={carregandoMotivosAusencia} tip="">
@@ -375,8 +416,8 @@ const ModalAnotacoesFrequencia = props => {
                   style={{ marginTop: '-15px' }}
                 >
                   {valoresIniciais &&
-                  valoresIniciais.auditoria &&
-                  valoresIniciais.auditoria.criadoPor ? (
+                    valoresIniciais.auditoria &&
+                    valoresIniciais.auditoria.criadoPor ? (
                     <Auditoria
                       criadoPor={valoresIniciais.auditoria.criadoPor}
                       criadoEm={valoresIniciais.auditoria.criadoEm}
@@ -460,10 +501,10 @@ ModalAnotacoesFrequencia.defaultProps = {
   componenteCurricularId: '',
   desabilitarCampos: false,
   exibirModal: false,
-  setExibirModal: () => {},
+  setExibirModal: () => { },
   dadosModal: [],
-  setDadosModal: () => {},
-  fechouModal: () => {},
+  setDadosModal: () => { },
+  fechouModal: () => { },
   listaPadraoMotivoAusencia: [],
 };
 
