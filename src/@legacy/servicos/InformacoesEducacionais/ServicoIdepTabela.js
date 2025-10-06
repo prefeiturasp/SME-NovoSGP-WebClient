7210eba0bd2bf12a9eb930d1a42f597619c3f232
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-10-06 09:58:20 -0300

    servicoIdepTabela criado

---
+++ src/@legacy/servicos/InformacoesEducacionais/ServicoIdepTabela.js
@@ -0,0 +1,20 @@
+import { URLSearchParams } from 'url';
+import api from '../api';
+
+class ServicoIdepTabela {
+  obterIdepTabela = (anoLetivo, codigoUe) => {
+    const params = new URLSearchParams();
+
+    if (codigoUe && String(codigoUe).trim() !== '-99')
+      params.append('codigoUe', codigoUe);
+
+    const queryString = params.toString();
+    const url = queryString
+      ? `/v1/painel-educacional/proficiencia-idep?${queryString}`
+      : '/v1/painel-educacional/proficiencia-idep';
+
+    return api.get(url);
+  };
+}
+
+export default new ServicoIdepTabela();
