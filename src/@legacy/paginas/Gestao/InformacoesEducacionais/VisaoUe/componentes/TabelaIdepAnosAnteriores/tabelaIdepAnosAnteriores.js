6ba6a61246df4bbfe01f593674f017bc6f2d4e1e
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-10-07 16:12:16 -0300

    estrutura inicial

---
+++ src/@legacy/paginas/Gestao/InformacoesEducacionais/VisaoUe/componentes/TabelaIdepAnosAnteriores/tabelaIdepAnosAnteriores.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import TabelaIdepAnosAnterioresDetalhes from './tabelaIdepAnosAnterioresDetalhes';
+import ServicoIdepTabela from '~/servicos/InformacoesEducacionais/ServicoIdepTabela';
+import { erros } from '~/servicos/alertas';
+import PropTypes from 'prop-types';
+import { Loader } from '~/componentes';
+
+export default function TabelaIdepAnosAnteriores({ ueCodigo }) {
+  const [dados, setDados] = useState([]);
+  const [carregando, setCarregando] = useState(false);
+
+  useEffect(() => {
+    const obterDados = async () => {
+      setCarregando(true);
+      try {
+        const response = await ServicoIdepTabela.obterIdepTabela(ueCodigo);
+
+        setDados(response.data);
+      } catch (error) {
+        erros(error);
+      } finally {
+        setCarregando(false);
+      }
+    };
+
+    obterDados();
+  }, [ueCodigo]);
+
+  if (carregando) {
+    return (
+      <div className="mt-4">
+        <Loader
+          loading={carregando}
+          className={carregando ? 'text-center' : ''}
+          tip="Carregando..."
+        />
+      </div>
+    );
+  }
+
+  return (
+    <TabelaIdepAnosAnterioresDetalhes dados={dados} carregando={carregando} />
+  );
+}
+
+TabelaIdepAnosAnteriores.propTypes = {
+  ueCodigo: PropTypes.string.isRequired,
+};
+
+TabelaIdepAnosAnteriores.defaultProps = {
+  ueCodigo: null,
+};
--- src/@legacy/paginas/Gestao/InformacoesEducacionais/VisaoUe/detalhesUnidadeEducacional.js
+++ src/@legacy/paginas/Gestao/InformacoesEducacionais/VisaoUe/detalhesUnidadeEducacional.js
@@ -1,8 +1,9 @@
+import { Col, Row } from 'antd';
 import PropTypes from 'prop-types';
 import { CardEstilizado } from '../shared/styles';
 import DetalhesUe from './componentes/DetalhesUe/detalhesUe';
 import TabelaIdep from './componentes/TabelaIdep/tabelaIdep';
-import { Col, Row } from 'antd';
+import TabelaIdepAnosAnteriores from './componentes/TabelaIdepAnosAnteriores/tabelaIdepAnosAnteriores';
 
 export default function DetalhesUnidadeEducacional({
   anoLetivo,
@@ -31,6 +32,11 @@ export default function DetalhesUnidadeEducacional({
           <Row gutter={[16, 16]}>
             <Col span={24}>
               <TabelaIdep anoLetivo={anoLetivo} ueCodigo={ueCodigo} />
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={24}>
+              <TabelaIdepAnosAnteriores ueCodigo={ueCodigo} />
             </Col>
           </Row>
         </div>
