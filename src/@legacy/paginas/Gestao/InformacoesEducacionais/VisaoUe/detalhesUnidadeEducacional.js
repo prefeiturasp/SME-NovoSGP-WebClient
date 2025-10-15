f50e981cd2f5973ad90844238d1cb452dd46ae1a
Flavio-Spassu <flavio.souza@spassu.com.br>
2025-10-15 14:07:21 -0300

    Adicionado import { Col, Row } from 'antd';

--- src/@legacy/paginas/Gestao/InformacoesEducacionais/VisaoUe/detalhesUnidadeEducacional.js
+++ src/@legacy/paginas/Gestao/InformacoesEducacionais/VisaoUe/detalhesUnidadeEducacional.js
@@ -4,6 +4,7 @@ import DetalhesUe from './componentes/DetalhesUe/detalhesUe';
 import TabelaIdep from './componentes/TabelaIdep/tabelaIdep';
 import TabelaAbandonoUe from './componentes/Abandono/tabelaAbandonoUe';
 import TabelaSondagemUe from './componentes/Sondagem/tabelaSondagemUe';
+import { Col, Row } from 'antd';
 
 export default function DetalhesUnidadeEducacional({
   anoLetivo,
