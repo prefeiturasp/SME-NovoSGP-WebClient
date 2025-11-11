2191b1c42aa1d39f928f8dfab6e5886c552af1b0
Felipe Piorini <felipemolica@hotmail.com>
2025-11-11 15:01:38 -0300

    fix: Remove linha em branco

--- src/pages/busca-ativa/registro-acoes/form/index.tsx
+++ src/pages/busca-ativa/registro-acoes/form/index.tsx
@@ -307,7 +307,6 @@ const BuscaAtivaRegistroAcoesForm: React.FC<BuscaAtivaRegistroAcoesFormProps> = 
                 />
               </Col>
 
-
               <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
                 <Button
                   color="primary"
