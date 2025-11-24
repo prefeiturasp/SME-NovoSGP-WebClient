b97ad29d8e3a8e285a11636d533110d9b216eb52
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-11-24 15:53:57 -0300

    novo tipoQuestao criado AB#138558

---
+++ src/@legacy/componentes-sgp/QuestionarioDinamico/Componentes/campoDinamicoAlertCheckbox.js
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setQuestionarioDinamicoEmEdicao } from '~/redux/modulos/questionarioDinamico/actions';
+
+const CampoDinamicoAlertCheckbox = props => {
+  const dispatch = useDispatch();
+  const { form, questaoAtual, desabilitado, onChange } = props;
+
+  const handleChange = e => {
+    form.setFieldValue(questaoAtual.id, e.target.checked);
+    dispatch(setQuestionarioDinamicoEmEdicao(true));
+    if (onChange) onChange();
+  };
+
+  return (
+    <div className="col-md-12 mb-3">
+      <div
+        className="d-flex align-items-center p-2 mb-2"
+        style={{ backgroundColor: '#FFF9E6', borderRadius: '4px' }}
+      >
+        <input
+          type="checkbox"
+          style={{ marginRight: '12px', marginLeft: '12px' }}
+          checked={!!form.values[questaoAtual.id]}
+          onChange={handleChange}
+          disabled={desabilitado}
+        />
+        <div>
+          <span
+            className="fw-bold"
+            style={{ color: 'red', fontWeight: 'bold' }}
+          >
+            {questaoAtual.nome}
+          </span>
+          <div className="small" style={{ color: 'red' }}>
+            {questaoAtual.observacao}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+CampoDinamicoAlertCheckbox.propTypes = {
+  form: PropTypes.oneOfType([PropTypes.any]),
+  questaoAtual: PropTypes.oneOfType([PropTypes.any]),
+  desabilitado: PropTypes.bool,
+  onChange: PropTypes.func,
+};
+
+CampoDinamicoAlertCheckbox.defaultProps = {
+  form: null,
+  questaoAtual: null,
+  desabilitado: false,
+  onChange: () => {},
+};
+
+export default CampoDinamicoAlertCheckbox;
