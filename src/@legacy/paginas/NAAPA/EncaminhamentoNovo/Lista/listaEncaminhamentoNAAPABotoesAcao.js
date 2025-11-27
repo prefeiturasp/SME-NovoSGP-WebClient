f83c08c0065a1b662a9574ff363b32639c008289
Lincoln <link7-7@hotmail.com>
2025-11-27 16:45:15 -0300

    botão dropDown com as 2 opções

--- src/@legacy/paginas/NAAPA/EncaminhamentoNovo/Lista/listaEncaminhamentoNAAPABotoesAcao.js
+++ src/@legacy/paginas/NAAPA/EncaminhamentoNovo/Lista/listaEncaminhamentoNAAPABotoesAcao.js
@@ -1,11 +1,12 @@
 import { ROUTES } from '@/core/enum/routes';
-import { Col, Row } from 'antd';
+import { Col, Row, Dropdown, Button as AntButton } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { Button, Colors } from '~/componentes';
 import BotaoVoltarPadrao from '~/componentes-sgp/BotoesAcaoPadrao/botaoVoltarPadrao';
 import { URL_HOME } from '~/constantes';
 import { SGP_BUTTON_NOVO } from '~/constantes/ids/button';
 import BtnImpressaoEncaminhamentoNAAPA from '../componentes/btnImpressaoNAAPA';
+import './listaEncaminhamentoNAAPABotoesAcao.css';
 
 const ListaEncaminhamentoNAAPABotoesAcao = props => {
   const navigate = useNavigate();
@@ -24,21 +25,43 @@ const ListaEncaminhamentoNAAPABotoesAcao = props => {
     });
   };
 
+  const items = [
+    {
+      key: '1',
+      label: (
+        <button className="btnDropNovoEcaminhamentoItem" onClick={onClickNovo}>
+          Encaminhamento Individual
+        </button>
+      ),
+    },
+    {
+      key: '2',
+      label: (
+        <button className="btnDropNovoEcaminhamentoItem" onClick={onClickNovo}>
+          Encaminhamento Institucional
+        </button>
+      ),
+    },
+  ];
+
   return (
     <Row gutter={[8, 8]} type="flex">
       <Col>
         <BotaoVoltarPadrao onClick={() => onClickVoltar()} />
       </Col>
       <Col>
-        <Button
-          bold
-          border
-          label="Novo Encaminhamento"
-          color={Colors.Azul}
-          id={SGP_BUTTON_NOVO}
-          onClick={onClickNovo}
-          disabled={desabilitarNovo}
-        />
+        <Dropdown
+          menu={{ items, selectable: false }}
+          placement="bottomLeft"
+          overlayClassName="dropdownNovoEncaminhamento"
+        >
+          <AntButton
+            disabled={desabilitarNovo}
+            className="btnDropNovoEcaminhamento"
+          >
+            Novo Encaminhamento
+          </AntButton>
+        </Dropdown>
       </Col>
     </Row>
   );
