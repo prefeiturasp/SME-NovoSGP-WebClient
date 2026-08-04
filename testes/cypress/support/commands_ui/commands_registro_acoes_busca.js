05adcbab3045e60e71276c7dce3f50b6569e66b7
Leandro S Castro <leandro2509@gmail.com>
2026-08-04 15:44:32 -0300

    fix: Atualiza senhas e melhora a estrutura dos testes de registro de ações

--- testes/cypress/support/commands_ui/commands_registro_acoes_busca.js
+++ testes/cypress/support/commands_ui/commands_registro_acoes_busca.js
@@ -1,82 +1,109 @@
-import Registro_Acoes_Busca_SGP_Localizadores from '../locators/registro_acoes_busca_locators'
+import Registro_Acoes_Busca_SGP_Localizadores from '../locators/registro_acoes_busca_locators';
 
-const registroAcoesBuscaSGPlocalizadores = new Registro_Acoes_Busca_SGP_Localizadores()
+const registroAcoesBuscaSGPlocalizadores = new Registro_Acoes_Busca_SGP_Localizadores();
 
-Cypress.on('uncaught:exception', () => false)
+Cypress.on('uncaught:exception', () => false);
 
 Cypress.Commands.add('acessar_registro_acoes_busca', () => {
-    cy.carregandoMenus()
-    
-    cy.get(registroAcoesBuscaSGPlocalizadores.menu_naapa()).should('be.visible').click()
-    cy.get(registroAcoesBuscaSGPlocalizadores.busca_ativa()).should('be.visible').click()
-    cy.get(registroAcoesBuscaSGPlocalizadores.menu_registro_acoes()).should('be.visible').click()
-    cy.url().should('include', '/busca-ativa/registro-acoes')
-})
+  cy.carregandoMenus();
+
+  cy.get(registroAcoesBuscaSGPlocalizadores.menu_naapa()).should('be.visible').click();
+  cy.get(registroAcoesBuscaSGPlocalizadores.busca_ativa()).should('be.visible').click();
+  cy.get(registroAcoesBuscaSGPlocalizadores.menu_registro_acoes()).should('be.visible').click();
+  cy.url().should('include', '/busca-ativa/registro-acoes');
+});
 
 Cypress.Commands.add('inserir_turma_registro_acoes_busca', (turma) => {
-    cy.get(registroAcoesBuscaSGPlocalizadores.campo_turma_registro_acoes_busca()).click({ force: true })
-    cy.wait(1000)
+  cy.get(registroAcoesBuscaSGPlocalizadores.campo_turma_registro_acoes_busca()).click({
+    force: true,
+  });
+  cy.wait(1000);
 
-    cy.get('body').then(($body) => {
-        if ($body.find(registroAcoesBuscaSGPlocalizadores.clica_turma_registro_acoes_busca()).length > 0) {
-            cy.get(registroAcoesBuscaSGPlocalizadores.clica_turma_registro_acoes_busca(), { timeout: 60000 })
-              .contains(turma.trim()).should('be.visible').click({ force: true })
-        } else {
-            cy.get(registroAcoesBuscaSGPlocalizadores.campo_turma_registro_acoes_busca()).click({ force: true })
-            cy.wait(1000)
-            cy.get(registroAcoesBuscaSGPlocalizadores.clica_turma_registro_acoes_busca(), { timeout: 60000 })
-              .contains(turma.trim()).should('be.visible').click({ force: true })
-        }
-    })
-
-    cy.get(registroAcoesBuscaSGPlocalizadores.campo_turma_registro_acoes_busca()).click()
-})
+  cy.get('body').then(($body) => {
+    if (
+      $body.find(registroAcoesBuscaSGPlocalizadores.clica_turma_registro_acoes_busca()).length > 0
+    ) {
+      cy.get(registroAcoesBuscaSGPlocalizadores.clica_turma_registro_acoes_busca(), {
+        timeout: 30000,
+      })
+        .contains(turma.trim())
+        .should('be.visible')
+        .click({ force: true });
+    } else {
+      cy.get(registroAcoesBuscaSGPlocalizadores.campo_turma_registro_acoes_busca()).click();
+      cy.wait(1000);
+      cy.get(registroAcoesBuscaSGPlocalizadores.clica_turma_registro_acoes_busca(), {
+        timeout: 30000,
+      })
+        .contains(turma.trim())
+        .should('be.visible')
+        .click({ force: true });
+    }
+  });
+});
 
 Cypress.Commands.add('inserir_periodo_registro_acoes_busca', () => {
-    cy.get(registroAcoesBuscaSGPlocalizadores.data_inicio_registro_acoes_busca()).click({ force: true })
-    cy.get(registroAcoesBuscaSGPlocalizadores.mes_atual_registro_acoes_busca()).click()
-    cy.get(registroAcoesBuscaSGPlocalizadores.mes_inicio_registro_acoes_busca()).click()
-    cy.get(registroAcoesBuscaSGPlocalizadores.dia_registro_acoes_busca()).click()
-    cy.get(registroAcoesBuscaSGPlocalizadores.data_fim_registro_acoes_busca()).click()
-    cy.get(registroAcoesBuscaSGPlocalizadores.hoje_registro_acoes_busca()).click()
-})
+  cy.get(registroAcoesBuscaSGPlocalizadores.data_inicio_registro_acoes_busca()).click({
+    force: true,
+  });
+  cy.get(registroAcoesBuscaSGPlocalizadores.mes_atual_registro_acoes_busca()).click();
+  cy.get(registroAcoesBuscaSGPlocalizadores.mes_inicio_registro_acoes_busca()).click();
+  cy.get(registroAcoesBuscaSGPlocalizadores.ano_registro_acoes_busca()).click();
+  cy.get(registroAcoesBuscaSGPlocalizadores.ano_inicio_registro_acoes_busca()).click();
+  cy.get(registroAcoesBuscaSGPlocalizadores.dia_registro_acoes_busca()).click();
+  cy.get(registroAcoesBuscaSGPlocalizadores.data_fim_registro_acoes_busca()).click();
+  cy.get(registroAcoesBuscaSGPlocalizadores.hoje_registro_acoes_busca()).click();
+});
 
 Cypress.Commands.add('selecionarContatoRegistroAcoesBusca', (tipoContato = 'ligacao') => {
   cy.get(registroAcoesBuscaSGPlocalizadores.campo_meio_contato_registro_acoes_busca())
     .should('be.visible')
-    .click()
+    .click();
 
   cy.get(registroAcoesBuscaSGPlocalizadores.dropdown_visivel(), { timeout: 5000 })
     .should('exist')
-    .and('be.visible')
+    .and('be.visible');
 
   let index;
   switch (tipoContato.toLowerCase()) {
     case 'ligacao':
-      index = 1
-      break
+      index = 1;
+      break;
     case 'visita':
-      index = 2
-      break
+      index = 2;
+      break;
     default:
-      throw new Error(`Tipo de contato inválido: "${tipoContato}". Use "ligacao" ou "visita".`)
+      throw new Error(`Tipo de contato inválido: "${tipoContato}". Use "ligacao" ou "visita".`);
   }
 
   cy.get(registroAcoesBuscaSGPlocalizadores.dropdown_visivel(), { timeout: 5000 })
     .find(registroAcoesBuscaSGPlocalizadores.opcao_meio_contato(index))
     .should('exist')
-    .click({ force: true })
-})
+    .click({ force: true });
+});
 
 Cypress.Commands.add('selecionar_contato_registro_acoes_busca', () => {
-  cy.selecionarContatoRegistroAcoesBusca('ligacao')
-})
+  cy.selecionarContatoRegistroAcoesBusca('ligacao');
+});
+
+Cypress.Commands.add('selecionar_ano_letivo_registro_acoes_busca', () => {
+  cy.get(registroAcoesBuscaSGPlocalizadores.ano_letivo_registro_acoes_busca())
+    .should('be.visible')
+    .click({ force: true });
+
+  cy.get(registroAcoesBuscaSGPlocalizadores.opcao_ano_letivo(), { timeout: 60000 })
+    .contains('2025')
+    .should('be.visible')
+    .click({ force: true })
+    .wait(2000);
+});
 
 Cypress.Commands.add('dados_carregados_registro_acoes_busca', (cenario) => {
-    if (cenario === "Sem dados encontrados") {
-        cy.get(registroAcoesBuscaSGPlocalizadores.tabela_dados_registro_acoes_busca()).should('exist')
-    } else {
-        cy.get(registroAcoesBuscaSGPlocalizadores.tabela_dados_registro_acoes_busca()).should('not.exist')
-    }
-})
-
+  if (cenario === 'Sem dados encontrados') {
+    cy.get(registroAcoesBuscaSGPlocalizadores.tabela_dados_registro_acoes_busca()).should('exist');
+  } else {
+    cy.get(registroAcoesBuscaSGPlocalizadores.tabela_dados_registro_acoes_busca()).should(
+      'not.exist',
+    );
+  }
+});
--- testes/cypress/support/locators/registro_acoes_busca_locators.js
+++ testes/cypress/support/locators/registro_acoes_busca_locators.js
@@ -1,20 +1,25 @@
 class Registro_Acoes_Busca_SGP_Localizadores {
-    menu_naapa = () => '.ant-menu-submenu-title:eq(1)'
-    busca_ativa = () => '.ant-menu-title-content:eq(2)'
-    menu_registro_acoes = () => '.ant-menu-item.ant-menu-item-only-child:eq(1)'
-    campo_turma_registro_acoes_busca = () => 'input[id="SGP_SELECT_TURMA"]'
-    clica_turma_registro_acoes_busca = () => '.ant-select-item-option-content'
-    tabela_dados_registro_acoes_busca = () => '.ant-table-placeholder > .ant-table-cell'
-    data_inicio_registro_acoes_busca = () => 'input[id="SGP_DATE_INICIO"]'
-    data_fim_registro_acoes_busca = () => 'input[id="SGP_DATE_FIM"]'
-    calendario_registro_acoes_busca = () => '.ant-picker-content'
-    mes_atual_registro_acoes_busca = () => '.ant-picker-month-btn'
-    mes_inicio_registro_acoes_busca = () => '.ant-picker-cell-inner:eq(0)'
-    dia_registro_acoes_busca = () => '.ant-picker-cell-inner:eq(3)'
-    hoje_registro_acoes_busca = () => '.ant-picker-today-btn:eq(1)'
-    campo_meio_contato_registro_acoes_busca = () => '#SGP_SELECT_ENTROU_CONTATO_FAMILIA_POR'
-    dropdown_visivel = () => '.ant-select-dropdown:not(.ant-select-dropdown-hidden)'    
-    opcao_meio_contato = (index) => `.ant-select-item.ant-select-item-option:eq(${index})`
+  menu_naapa = () => '.ant-menu-submenu-title:eq(1)';
+  busca_ativa = () => '.ant-menu-title-content:eq(2)';
+  menu_registro_acoes = () => '.ant-menu-item.ant-menu-item-only-child:eq(1)';
+  campo_turma_registro_acoes_busca = () => 'input[id="SGP_SELECT_TURMA"]';
+  clica_turma_registro_acoes_busca = () => '.ant-select-item-option-content';
+  tabela_dados_registro_acoes_busca = () => '.ant-table-placeholder > .ant-table-cell';
+  data_inicio_registro_acoes_busca = () => 'input[id="SGP_DATE_INICIO"]';
+  data_fim_registro_acoes_busca = () => 'input[id="SGP_DATE_FIM"]';
+  calendario_registro_acoes_busca = () => '.ant-picker-content';
+  mes_atual_registro_acoes_busca = () => '.ant-picker-month-btn';
+  mes_inicio_registro_acoes_busca = () => '.ant-picker-cell-inner:eq(1)';
+  ano_registro_acoes_busca = () => '.ant-picker-year-btn';
+  ano_inicio_registro_acoes_busca = () => '.ant-picker-cell-inner:eq(5)';
+  dia_registro_acoes_busca = () => '.ant-picker-cell-inner:eq(3)';
+  hoje_registro_acoes_busca = () => '.ant-picker-today-btn:eq(1)';
+  campo_meio_contato_registro_acoes_busca = () => '#SGP_SELECT_ENTROU_CONTATO_FAMILIA_POR';
+  dropdown_visivel = () => '.ant-select-dropdown:not(.ant-select-dropdown-hidden)';
+  opcao_meio_contato = (index) => `.ant-select-item.ant-select-item-option:eq(${index})`;
+  opcao_ano_letivo = () =>
+    '.ant-select-dropdown:not(.ant-select-dropdown-hidden) .ant-select-item-option-content';
+  ano_letivo_registro_acoes_busca = () => 'input[id="SGP_SELECT_ANO_LETIVO"]';
 }
 
-export default Registro_Acoes_Busca_SGP_Localizadores
+export default Registro_Acoes_Busca_SGP_Localizadores;
